b21f693f4889fe56a7f893cf2412500ce36b20bf
Farid Adam <faridibin@gmail.com>
2023-09-24 22:31:26 -0400

    Create Splash.js

---
+++ src/components/Splash.js
@@ -0,0 +1,9 @@
+const Loading = () => {
+	return (
+		<div className="flex justify-center items-center h-152 rounded-lg">
+			<h1 className="text-3xl text-black">SuperQuizApp</h1>
+		</div>
+	);
+};
+
+export default Loading;
